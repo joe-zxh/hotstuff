75ceb95a5b2384c798a6cf84dc3acfde02ee870c
zouLab <178559145@qq.com>
2020-11-26 19:43:25 +0800

    c

--- cmd/genSchedule/main.go
+++ cmd/genSchedule/main.go
@@ -2,11 +2,27 @@ package main
 
 import "fmt"
 
-func main() {
-	clusterSize := 52
+var clusterSize int = 160
+
+func genSchedule() {
 	fmt.Printf("[")
 	for i := 1; i < clusterSize; i++ {
 		fmt.Printf("%d, ", i)
 	}
 	fmt.Printf("%d]\n", clusterSize)
+}
+
+func genReplicas() {
+	for i := 1; i <= clusterSize; i++ {
+		fmt.Printf("[[replicas]]\n")
+		fmt.Printf("id = %d\n", i)
+		fmt.Printf("peer-address = \"127.0.0.1:%d\"\n", 13370+i)
+		fmt.Printf("client-address = \"127.0.0.1:%d\"\n", 23370+i)
+		fmt.Printf("pubkey = \"keys/r%d.key.pub\"\n", i)
+		fmt.Printf("cert = \"keys/r%d.crt\"\n\n", i)
+	}
+}
+
+func main() {
+	genReplicas()
 }
